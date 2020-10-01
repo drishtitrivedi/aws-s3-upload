0092e0efcade11074f6017efbb7b353a8dad746a
Drishti Trivedi <45985420+drishtitrivedi@users.noreply.github.com>
2020-10-01 12:13:38 -0700

    Update config.php

--- php-backend/app/config.php
+++ php-backend/app/config.php
@@ -3,8 +3,8 @@
 // set up configuration for AWS s3 bucket
 return  [
     's3' => [
-        'key' => 'AKIAJBIFQHEW3LGOYEJQ',
-        'secret' => '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',
+        'key' => 'key',
+        'secret' => 'secret',
         'bucket' => 'drishti.bucket'
     ]
 ]; 
